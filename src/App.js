9140b9194409862ad3b143b221f30bb3a0c5ca97
anglmugabe <anglmugabe@gmail.com>
2024-12-22 20:23:15 +0200

    added form component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
 	return (
 		<div className="App">
 			<div className="container">
-				<header className="App-header"></header>
+				<header className="App-form">
+					<Dictionary />
+				</header>
 
-				<footer>
+				<footer className="App-footer">
 					{" "}
 					This website was coded by <a href=""> Angela Mugabe </a>, is{" "}
 					<a href="https://github.com/anglmugabe/minideictionary-react">
